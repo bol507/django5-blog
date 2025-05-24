6cc3cce060e5b19880078673709051536094cd6b
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-05-23 20:37:11 -0500

    feat: implementing tagging using django-taggit

--- myapp/views.py
+++ myapp/views.py
@@ -5,25 +5,33 @@ from django.core.paginator import Paginator, EmptyPage,  PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
-def post_list(request):
-      post_list = Post.published.all()
-      # Pagination with 3 posts per page
-      paginator = Paginator(post_list, 3)
-      page_number = request.GET.get('page', 1)
-      try:
+def post_list(request, tag_slug=None):
+        post_list = Post.published.all()
+        tag = None
+        if tag_slug:
+            tag = get_object_or_404(Tag, slug=tag_slug)
+            post_list = post_list.filter(tags__in=[tag])
+        # Pagination with 3 posts per page
+        paginator = Paginator(post_list, 3)
+        page_number = request.GET.get('page', 1)
+        try:
             posts = paginator.page(page_number)
-      except PageNotAnInteger:
+        except PageNotAnInteger:
             # If page_number is not an integer get the first page
             posts = paginator.page(1)
-      except EmptyPage:
+        except EmptyPage:
             # If page_number is out of range get last page of results
             posts = paginator.page(paginator.num_pages)
-      return render(
+        return render(
             request,
             'blog/post/list.html',
-            {'posts': posts}
-      )
+            {
+                'posts': posts,
+                'tag': tag
+            }
+        )
 
 def post_detail(request, year,month,day,post):
     post = get_object_or_404(
